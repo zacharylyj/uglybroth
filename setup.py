60105db70cab36986dd2eee7fd7c0b40a31e7865
Zachary Leong <zacharylyj@gmail.com>
2024-04-03 14:25:07 +0800

    rah

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-your-username",
+    name="zacharylyj",
     version="0.0.1",
     author="BrothLover",
     author_email="zacharylyj@gmail.com",
     description="Are you a beautiful soup hater??? Well boy do I have the package for you. Introducing uglybroth, the ULTIMATE rival of beautiful soup.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/zacharylyj/uglybroth.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
