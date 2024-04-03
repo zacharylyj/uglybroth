8f6614973d8740a378369705a8d28f97a906a5ec
Zachary Leong <zacharylyj@gmail.com>
2024-04-03 14:20:11 +0800

    rah

---
+++ broth.py
@@ -0,0 +1,1 @@
+print("Soup bad Broth good")
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-your-username",
+    version="0.0.1",
+    author="BrothLover",
+    author_email="zacharylyj@gmail.com",
+    description="Are you a beautiful soup hater??? Well boy do I have the package for you. Introducing uglybroth, the ULTIMATE rival of beautiful soup.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
